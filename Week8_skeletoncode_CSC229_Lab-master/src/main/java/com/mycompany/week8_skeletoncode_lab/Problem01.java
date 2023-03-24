6582e0056323d9b60ce9c75517858e7322c2d033
1cgul <cgulino03@gmail.com>
2023-03-24 18:56:37 -0400

    problem01 finished

--- Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
+++ Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
@@ -10,11 +10,28 @@ package com.mycompany.week8_skeletoncode_lab;
  */
 public class Problem01 {
     
-    public static long getSumOfPrimes(int n){
-    // Todo 04: Develop a method that returns the sum of the prime numbers between 1 and n
-    //          Test your solution
-    //          Analyze its space and time  
+    public static long getSumOfPrimes(int n){// Todo 04: Develop a method that returns the sum of the prime numbers between 1 and n
+    long sum = 0;
+    for(int i = 2; i <= n; i++){
+        boolean isPrimeNum = true;
+        for(int j = 2; j <=Math.sqrt(i); j++){
+            if (i%j ==0){
+                isPrimeNum = false;
+                break;
+            }
+        }
+        if(isPrimeNum){
+            sum+=i;
+        }
+    }
+    return sum;
     
+    }//          Analyze its space and time  
+    // space complexity is O(1) because its not creating reference types like String, Integer, etc
+    // time complexity is O(n*logn) because the outer loop is O(n) and the inner is O(logn)
+    public static void main(String[] args){
+        int target = 49;
+        System.out.println(getSumOfPrimes(target)); //          Test your solution
     }
     
 }
