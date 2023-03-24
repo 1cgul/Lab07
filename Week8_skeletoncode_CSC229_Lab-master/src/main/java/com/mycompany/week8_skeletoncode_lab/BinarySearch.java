b719affa12710d929d74262c99b955b0b5509532
1cgul <cgulino03@gmail.com>
2023-03-24 18:28:46 -0400

    binarysearch and bubblesort done

--- Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
+++ Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
@@ -29,5 +29,28 @@ public class BinarySearch {
     }
     //ToDo 2: Call the above method and test the algorithm  
     // provide time and space analysis 
+    public static void main (String[] args){
+        int target = 8;
+        int lower = 0;
+        int upper = 9;
+        int[] x = new int[10];
+        x[0] = 0;
+        x[1] = 1;
+        x[2] = 2;
+        x[3] = 3;
+        x[4] = 4;
+        x[5] = 5;
+        x[6] = 6;
+        x[7] = 7;
+        x[8] = 8;
+        x[9] = 9;
+
+        System.out.println(runBinarySearchIteratively(x, target, lower, upper));
+
+        //time complexity is O(log(n))
+       
+        
+        
+    }
 
 }
--- Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
+++ Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.week8_skeletoncode_lab;
 
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
 /**
  *
  * @author MoaathAlrajab
@@ -15,8 +17,28 @@ public class BubbleSort {
         for (outer = size - 1; outer > 0; outer--) { // counting down
             for (inner = 0; inner < outer; inner++) { // bubbling up
                 //ToDo 3: complete this algorithm, test it, provide its time complexity
+                if(a[inner] > a[inner+1]){
+                    temp= a[inner];
+                    a[inner]= a[inner+1];
+                    a[inner+1]=temp;
+                }
             }
-        }
+        
+         }
+       for(int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        } 
+    }
+    public static void main(String[] args){
+
+        int[] a = new int[5];
+        a[0] = 5;
+        a[1] = 4;
+        a[2] = 24;
+        a[3] = 41;
+        a[4] = 15;
+        bubbleSort(a, a.length);
+        
     }
     
     
