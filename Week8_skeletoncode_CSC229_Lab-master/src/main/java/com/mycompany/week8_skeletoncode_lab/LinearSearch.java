2ce81b43e7b2cce70eecc077772c23286e9a9391
1cgul <cgulino03@gmail.com>
2023-03-24 18:45:10 -0400

    linearsearch done

--- Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
+++ Week8_skeletoncode_CSC229_Lab-master/src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
@@ -14,7 +14,26 @@ public class LinearSearch {
     {
         int n = arr.length;
         // Todo 01: - complete the implementation of linear search and test your code  
+        for(int i = 0; i < n; i++){
+            if(arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
          //         - prvoide asymptotic analysis of the developed solution
+         // Big O = O(n), upper bound and lower bound = n so big theta = theta(n);
+    }
+
+    public static void main(String[] args){
+        int target = 2;
+        int[] arr = new int[5];
+        arr[0] = 5;
+        arr[1] = 9;
+        arr[2] = 1;
+        arr[3] = 2;
+        arr[4] = 6;
+
+        System.out.println("Target " + target + " found at index " + search(arr, target)+ ".");
     }
     
 }
